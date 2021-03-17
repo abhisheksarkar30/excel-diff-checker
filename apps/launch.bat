aea4bebf350b3be9026c4b3fcea618976ea1caee
abhisheksarkar30 <8725485+abhisheksarkar30@users.noreply.github.com>
2021-03-17 19:46:49 +0530

    modified launch script for both way comparison

--- apps/launch.bat
+++ apps/launch.bat
@@ -1,2 +1,5 @@
 "%java8home%/bin/java" -jar excel-diff-checker.jar -b old.* -t new.* %* > edc.log
 pause
+
+"%java8home%/bin/java" -jar excel-diff-checker.jar -b new.* -t old.* %* > edc-opp.log
+pause
